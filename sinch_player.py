b961c0aebdf18b06a0e371a0e4309e9fc29c42ae
Spencer Whitt <spencerlwhitt@gmail.com>
2012-11-26 16:08:56 -0500

    Load db in playlist, close connection on exit

--- sinch_player.py
+++ sinch_player.py
@@ -2,6 +2,8 @@
 import Tkinter as tk
 import mpd
 import sys
+from pprint import pprint
+from random import choice
 from socket import error as SocketError
 from grid import Grid
 
@@ -9,18 +11,31 @@ from grid import Grid
 HOST = 'localhost'
 PORT = '6600'
 
+volume = 100
 client = mpd.MPDClient()
-
 try:
     client.connect(host=HOST, port=PORT)
 except SocketError:
     print "Failed to connect to MPD. Are you sure it's installed and running on port %s?" % PORT
     sys.exit(1)
 
-client.update()
+# Don't shuffle randomly (better for demo if we don't)
+client.random(0)
+# Repeat the playlist
+client.repeat(1)
 
-print client.status()
-print client.stats()
+pprint(client.status())
+pprint(client.stats())
+#pprint(client.idle('player'))
+
+# Check database for any updates
+client.update()
+# Clear current playlist
+client.clear()
+# Add entire database to playlist
+client.add("/")
+
+pprint(client.playlistinfo())
 
 def play():
     status = client.status()
@@ -30,6 +45,8 @@ def play():
         client.play()
 
 root = tk.Tk()
+
+tk.Label(root, text="A label, outside the grid").pack()
 
 # Create an accessible button grid
 buttons = [ {'text': 'Play',           'command': play }
@@ -41,4 +58,11 @@ buttons = [ {'text': 'Play',           'command': play }
 
 Grid(root, 2, 3, buttons).pack()
 
+# Closing the window also closes connection to mpd
+def close():
+    client.close()
+    root.destroy()
+root.protocol("WM_DELETE_WINDOW", close)
+
+# Start gui loop
 root.mainloop()
